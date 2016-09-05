e2a3760c91f492699338b10f752bea27d6cf1893
Wataru.Kasahara <kasahara1019@gmail.com>
2016-09-06 07:45:44 +0900

    temp

---
+++ containers/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Test</h1>
+      </div>
+    )
+  }
+}
---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+
+import App from '../containers/App'
+
+ReactDOM.render(
+  <App />,
+  document.getElementById("root")
+)
